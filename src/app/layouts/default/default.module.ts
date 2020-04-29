f957eaa6520d117a37c29dcee2be37f32eed47ca
Juan José Fernández <juanjofer1999@gmail.com>
2020-04-28 20:06:27 -0400

    conflictos solucionados

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,33 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DefaultComponent } from './default.component';
-import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
-import { LoginComponent } from 'src/app/modules/login/login.component';
-import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-=======
-import {MatButtonModule} from '@angular/material/button';
-
-
->>>>>>> d363fc9d911b342ae8bd524148392267f109f6bf
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DefaultComponent } from "./default.component";
+import { DashboardComponent } from "src/app/modules/dashboard/dashboard.component";
+import { LoginComponent } from "src/app/modules/login/login.component";
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
-    DefaultComponent ,
+    DefaultComponent,
     //DashboardComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
-<<<<<<< HEAD
     BrowserAnimationsModule,
     MatFormFieldModule,
-=======
-    MatButtonModule
->>>>>>> d363fc9d911b342ae8bd524148392267f109f6bf
-  ]
+  ],
 })
-export class DefaultModule { }
+export class DefaultModule {}
