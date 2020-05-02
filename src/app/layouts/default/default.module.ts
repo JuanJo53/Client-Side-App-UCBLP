170052878b826c4e0fd9fc6b053724c1f4494173
Sergio <si.prudencio@acad.ucb.edu.bo>
2020-05-02 18:05:06 -0400

    reparando

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,22 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DefaultComponent } from './default.component';
-import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
-import { LoginComponent } from 'src/app/modules/login/login.component';
-import { RouterModule } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DefaultComponent } from "./default.component";
 
-
+import { LoginComponent } from "src/app/modules/login/login.component";
+import { RouterModule } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { DashboardComponent } from "src/app/modules/dashboard/dashboard.component";
+import { PostsComponent } from "src/app/modules/posts/posts.component";
+import { SharedModule } from "src/app/shared/shared.module";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [
-    DefaultComponent ,
-    //DashboardComponent,
-    LoginComponent
+    DefaultComponent,
+    DashboardComponent,
+    LoginComponent,
+    PostsComponent,
   ],
   imports: [
     CommonModule,
@@ -25,7 +28,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatFormFieldModule,
     MatInputModule,
     FlexLayoutModule,
-    MatToolbarModule
-  ]
+    MatToolbarModule,
+    SharedModule,
+    MatSidenavModule,
+  ],
 })
-export class DefaultModule { }
+export class DefaultModule {}
