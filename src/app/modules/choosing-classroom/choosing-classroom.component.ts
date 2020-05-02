0255e150ee70cc27c8061ec0915936c8d4800e4b
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-02 18:30:51 -0400

    choosing classroom files created

---
+++ src/app/modules/choosing-classroom/choosing-classroom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choosing-classroom',
+  templateUrl: './choosing-classroom.component.html',
+  styleUrls: ['./choosing-classroom.component.scss']
+})
+export class ChoosingClassroomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
