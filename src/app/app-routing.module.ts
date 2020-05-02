64a2f5dc6a46ffb3c5d6e454770a8035b2a547ec
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-02 19:48:08 -0400

    sidebar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DefaultComponent } from './layouts/default/default.component';
-import { LoginComponent } from './modules/login/login.component';
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DefaultComponent } from "./layouts/default/default.component";
+import { LoginComponent } from "./modules/login/login.component";
+import { DashboardComponent } from "./modules/dashboard/dashboard.component";
+import { PostsComponent } from "./modules/posts/posts.component";
 
-
-
-const routes: Routes = [{
-  path:'',
-  component:DefaultComponent,
-  children :[{
-    path:'',
-    component : LoginComponent
-  }]
-}];
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultComponent,
+    children: [
+      {
+        path: "",
+        //component : LoginComponent
+        component: DashboardComponent,
+      },
+      {
+        path: "posts",
+        component: PostsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
