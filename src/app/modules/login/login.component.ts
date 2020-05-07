6c73905358d23c2e4449bffbf3c8f098fd344407
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-07 15:28:54 -0400

    comentario agregados para backend => listo para backend

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  mylogo:string = "assets/logo.png";
-  constructor() { }
-
-  ngOnInit() {
+  //-----variables
+  textoLogo: string = "UCB English";
+  imagenLogo: string = "assets/logo.png";
+  usuario = "teacher";
+  correoAcademicoUsuario: string;
+  contraseniaUsuario: string;
+  //-----#variables
+  //-----funciones
+  btnLogin() {
+    console.log("login button presionado");
+    console.log(this.correoAcademicoUsuario);
+    console.log(this.contraseniaUsuario);
   }
-
+  //-----#funciones
+  constructor() {}
+  ngOnInit() {}
 }
