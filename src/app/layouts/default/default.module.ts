9d33786a7bae0722019818b244d61c5ef6384453
Juan José Fernández <juanjofer1999@gmail.com>
2020-05-13 22:02:34 -0400

    solucion de conflicto en default.module.ts

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -9,15 +9,15 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
-
 import { PostsComponent } from "src/app/modules/posts/posts.component";
 import { SharedModule } from "src/app/shared/shared.module";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatIconModule } from "@angular/material/icon";
 import { FormsModule } from "@angular/forms";
-//Revisar este import! Me saltaba error en la linea 35! Revisar tambien los demas imports en otras librerias!
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DashboardComponent } from "src/app/modules/dashboard/dashboard.component";
+import { ThemesComponent } from "src/app/modules/themes/themes.component";
 
 @NgModule({
   declarations: [
