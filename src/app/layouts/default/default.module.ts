d6e9a412e2379e4b85b52b51b2832f602e58afd0
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-16 18:19:41 -0400

    se agrego flex grid layout = > modals

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -8,7 +8,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { FlexLayoutModule } from "@angular/flex-layout";
+//import { FlexLayoutModule } from "@angular/flex-layout";
 import { PostsComponent } from "src/app/modules/posts/posts.component";
 import { SharedModule } from "src/app/shared/shared.module";
 import { MatSidenavModule } from "@angular/material/sidenav";
@@ -16,13 +16,8 @@ import { MatMenuModule } from "@angular/material/menu";
 import { MatIconModule } from "@angular/material/icon";
 import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-<<<<<<< HEAD
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { ThemesComponent } from 'src/app/modules/themes/themes.component';
-=======
-import { DashboardComponent } from "src/app/modules/dashboard/dashboard.component";
-import { ThemesComponent } from "src/app/modules/themes/themes.component";
->>>>>>> 9d33786a7bae0722019818b244d61c5ef6384453
 
 @NgModule({
   declarations: [
@@ -39,7 +34,7 @@ import { ThemesComponent } from "src/app/modules/themes/themes.component";
     // BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    FlexLayoutModule,
+    //FlexLayoutModule,
     MatToolbarModule,
     SharedModule,
     MatSidenavModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,7 @@ import { MatDividerModule } from "@angular/material/divider";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
-import { FlexLayoutModule } from "@angular/flex-layout";
+//import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatListModule } from "@angular/material/list";
 import { RouterModule } from "@angular/router";
@@ -20,7 +20,7 @@ import { MatExpansionModule } from "@angular/material/expansion";
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    FlexLayoutModule,
+    // FlexLayoutModule,
     MatMenuModule,
     MatListModule,
     RouterModule,
