60904a29039d081c2b4efda83c2a9e7ad05ed60b
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-19 21:54:13 -0400

    style fixed

--- src/app/modules/choosing-classroom/choosing-classroom.component.ts
+++ src/app/modules/choosing-classroom/choosing-classroom.component.ts
@@ -3,8 +3,9 @@ import { Teacher } from "src/app/models/Teacher";
 import { AuthService } from "src/app/_services/general_services/auth.service";
 import { UserService } from "src/app/_services/general_services/user.service";
 import { TokenStorageService } from "src/app/_services/general_services/token-storage.service";
-import { Router, ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
 import { CardClassroom } from "src/app/models/CardClassroom";
+
 @Component({
   selector: "app-choosing-classroom",
   templateUrl: "./choosing-classroom.component.html",
@@ -34,6 +35,13 @@ export class ChoosingClassroomComponent implements OnInit {
       totalEstudiantes: 30,
       semestre: "1/2020",
     },
+    {
+      materia: "English 3",
+      diasMateria: "Mon - Tue - Wed - Thu",
+      horarioMateria: "14:30 - 16:00",
+      totalEstudiantes: 30,
+      semestre: "1/2020",
+    },
   ];
 
   userDocente: Teacher = {
@@ -53,30 +61,24 @@ export class ChoosingClassroomComponent implements OnInit {
 
   //-----#funciones-----
   constructor(
-    private usService: ActivatedRoute,
+    private usService: UserService,
     private tokenServ: TokenStorageService,
     private router: Router
   ) {}
 
   ngOnInit() {
+    this.numeroCards = this.classroomCards.length;
+    console.log("total : " + this.numeroCards);
     if (this.tokenServ.getToken() === "undefined") {
       this.router.navigate(["/"]);
       return false;
     } else {
-      this.usService.data.subscribe({
+      this.usService.getProfileDocente().subscribe({
         next: (data) => {
-          console.log(data.classroom);
-          this.userDocente.correoDocente = data.profile.correo_docente;
+          this.userDocente.correoDocente = data.correo_docente;
           this.userDocente.nombreDocente =
-            data.profile.nombre_docente +
-            " " +
-            data.profile.ap_pat_docente +
-            " " +
-            data.profile.ap_mat_docente;
-          this.userDocente.primaLetra = data.profile.nombre_docente.substring(
-            0,
-            1
-          );
+            data.nombre_docente + data.ap_pat_docente + data.ap_mat_docente;
+          this.userDocente.primaLetra = data.nombre_docente.substring(0, 1);
         },
         error: (err) => {
           this.router.navigate(["/"]);
