259ddd9606f936c52a02857a6738944ccd8d6e62
ariel colque <ariel.colque.herrera@gmail.com>
2020-05-30 18:29:09 -0400

    navbar progress bar added

--- src/app/modules/dialogs/create-practice/initial-information/initial-information.component.ts
+++ src/app/modules/dialogs/create-practice/initial-information/initial-information.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-initial-information',
-  templateUrl: './initial-information.component.html',
-  styleUrls: ['./initial-information.component.scss']
+  selector: "app-initial-information",
+  templateUrl: "./initial-information.component.html",
+  styleUrls: ["./initial-information.component.scss"],
 })
 export class InitialInformationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  next() {
+    console.log("next");
   }
-
+  previous() {
+    console.log("previous");
+  }
 }
