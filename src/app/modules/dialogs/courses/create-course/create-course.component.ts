f45ac6d99692fe76a9aea356f9e96f992fe69406
ariel colque <arielcolque444@gmail.com>
2020-07-05 17:57:31 -0400

    dialog schedule finalized

--- src/app/modules/dialogs/courses/create-course/create-course.component.ts
+++ src/app/modules/dialogs/courses/create-course/create-course.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-
+export class HorarioClase {
+  dia: string;
+  horaComienzo: string;
+  horaFinaliza: string;
+}
 @Component({
   selector: "app-create-course",
   templateUrl: "./create-course.component.html",
@@ -8,10 +12,62 @@ import { Component, OnInit } from "@angular/core";
 export class CreateCourseComponent implements OnInit {
   nombreClassroom: string;
   valueQuantity: number;
-  cantidadEstudiantes: number[]=[1,2,3];
+  cantidadEstudiantes: number[] = [1, 2, 3];
+  diasSemana: string[] = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  valueDiaSemana: string;
+  fechaStart: string;
+  fechaEnd: string;
+  horarioClase: HorarioClase[] = [
+    {
+      dia: "",
+      horaComienzo: "",
+      horaFinaliza: "",
+    },
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
-  guardarCurso(){}
+  guardarCurso() {
+    console.log(
+      "cantidad : " +
+        this.valueQuantity +
+        "\n" +
+        "nombre curso :" +
+        this.nombreClassroom
+    );
+
+    for (var item of this.horarioClase) {
+      console.log(
+        "dia:" +
+          item.dia +
+          "\n" +
+          "hora Start : " +
+          item.horaComienzo +
+          "\n" +
+          "hora End:" +
+          item.horaFinaliza
+      );
+    }
+  }
+  eliminarHorario(index) {
+    console.log("eliminar");
+    this.horarioClase.splice(index, 1);
+  }
+  agregarHorario() {
+    var aux: HorarioClase = {
+      dia: "",
+      horaComienzo: "",
+      horaFinaliza: "",
+    };
+    console.log("agregar");
+    this.horarioClase.push(aux);
+  }
 }
