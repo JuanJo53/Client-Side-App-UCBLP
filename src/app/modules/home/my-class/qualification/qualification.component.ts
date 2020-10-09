f468e1c38471ff57589fa2f0af2003a30e42c061
ArielColqueH <arielcolque444@gmail.com>
2020-10-09 12:46:19 -0400

    tables design fixed

--- src/app/modules/home/my-class/qualification/qualification.component.ts
+++ src/app/modules/home/my-class/qualification/qualification.component.ts
@@ -34,6 +34,25 @@ export interface ListaDeNotas {
   promedioFinal: number;
   id: number;
 }
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA2: PeriodicElement[] = [
+  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
+  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
+  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
+  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
+  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
+  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
+  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
+  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
+  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
+  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
+];
 
 @Component({
   selector: "app-qualification",
@@ -42,6 +61,8 @@ export interface ListaDeNotas {
   encapsulation: ViewEncapsulation.None,
 })
 export class QualificationComponent implements OnInit {
+  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
+  dataSource2 = ELEMENT_DATA2;
   positionOptions: TooltipPosition[] = [
     "after",
     "before",
@@ -58,7 +79,7 @@ export class QualificationComponent implements OnInit {
   columnsPor: string[] = [];
 
   ELEMENT_DATA: ListaEstudiante[] = [];
-  displayedColumns: string[] = ["posicion", "p_nombre", "m_nombre", "nombre"];
+  displayedColumns2: string[] = ["posicion", "p_nombre", "m_nombre", "nombre"];
   dataSource = new MatTableDataSource(this.ELEMENT_DATA);
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
