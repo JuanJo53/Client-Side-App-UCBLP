e89394873faf168ab23ea907bc51cc8239487e25
ArielColqueH <arielcolque444@gmail.com>
2020-11-20 18:32:42 -0400

    second step redone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { Component } from "@angular/core";
+import { MatSidenav } from "@angular/material/sidenav";
+import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'ucbEnglish';
+  title = "ucbEnglish";
 }
